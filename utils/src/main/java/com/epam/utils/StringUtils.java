22a625d7eff43471b929b19d09c742d6e64b3bc1
IlyaBrutski <101987935+IlyaBrutski@users.noreply.github.com>
2023-06-22 17:19:50 +0300

    Maven

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,18 @@
 package com.epam.utils;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (isBlank(str)) {
+            return false;
+        }
+
+        try {
+            double num = Double.parseDouble(str);
+            return num > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
