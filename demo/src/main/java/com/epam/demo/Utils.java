ae206a677a184bd8097a7cd070b0354afbcb8b56
IlyaBrutski <101987935+IlyaBrutski@users.noreply.github.com>
2023-08-16 14:59:45 +0300

    maven

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,15 +1,25 @@
 package com.epam.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
-        return args.stream()
-                .allMatch(str -> str.matches("\\d+"))
-                && args.stream()
-                .map(Integer::parseInt)
-                .allMatch(num -> num > 0);
+        try{
+            for (String arg: args) {
+                double numb = Double.parseDouble(arg);
+                if(numb<=0) return false;
+            }
+        }
+        catch(Exception e){
+            return false;
+        }
+        return true;
     }
-    
+
+    public static void main(String[] args) {
+        List<String> lst = new ArrayList<>();
+        lst.add(null);
+        System.out.println(isAllPositiveNumbers(lst));
+    }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,18 +1,29 @@
 package com.epam.utils;
 
-import static org.apache.commons.lang3.StringUtils.isBlank;
-
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (isBlank(str)) {
+        try{
+            if (str.contains(" ")) return false;
+            double numb = Double.parseDouble(str);
+            if(numb<=0) return false;
+        }
+        catch(Exception e){
             return false;
         }
+        return true;
+//        return org.apache.commons.lang3.StringUtils.isNumeric(str);
+    }
 
-        try {
-            double num = Double.parseDouble(str);
-            return num > 0;
-        } catch (NumberFormatException e) {
-            return false;
-        }
+    public static void main(String[] args) {
+        System.out.println(isPositiveNumber(""));
+        System.out.println(isPositiveNumber("-6"));
+        System.out.println(isPositiveNumber("0"));
+        System.out.println(isPositiveNumber("3"));
+        System.out.println(isPositiveNumber("12.2"));
+        System.out.println(isPositiveNumber("-9.5"));
+        System.out.println(isPositiveNumber("."));
+        System.out.println(isPositiveNumber("0.00"));
+        System.out.println(isPositiveNumber("  100.1"));
+        System.out.println(isPositiveNumber(null));
     }
 }
